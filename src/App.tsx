a5d5f7915b7b1e7255b3407fbd50d64b877301d7
hyorish03 <hyorish2001@gmail.com>
2023-07-14 01:03:31 +0900

    Feat: 리듀서, 액션, 스토어 구현 및 카운터 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+type Props = {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
+function App({ value, onIncrement, onDecrement }: Props) {
+  return (
+    <>
+      Clicked:{value}times
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createStore } from "redux";
+import counter from "./reducers/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+      />
+    </React.StrictMode>
+  );
+};
+
+render();
+
+store.subscribe(render);
