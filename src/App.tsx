f569a9ce1e9cbb298b6cb454fd416e591c46ea99
hyorish03 <hyorish2001@gmail.com>
2023-07-14 18:32:21 +0900

    Feat: comebineReducers 및 Redux Provider 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
+import react, { useState } from "react";
+
 type Props = {
-  value: number;
+  value: any;
   onIncrement: () => void;
   onDecrement: () => void;
 };
 function App({ value, onIncrement, onDecrement }: Props) {
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); //페이지 리프레쉬 방지
+    setTodoValue("");
+  };
+  console.log(todoValue);
+
   return (
     <>
-      Clicked:{value}times
+      {/* Clicked:{value}times */}
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createStore } from "redux";
-import counter from "./reducers/index.tsx";
+import rootReducer from "./reducers/index.tsx";
+import { Provider } from "react-redux";
 
-const store = createStore(counter);
-
+const store = createStore(rootReducer);
+store.dispatch({ type: "ADD_TODO", text: "useRedux" });
+console.log(store.getState());
 const render = () => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <App
-        value={store.getState()}
-        onIncrement={() => store.dispatch({ type: "INCREMENT" })}
-        onDecrement={() => store.dispatch({ type: "DECREMENT" })}
-      />
+      <Provider store={store}>
+        <App
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+          onDecrement={() => store.dispatch({ type: "DECREMENT" })}
+        />
+      </Provider>
     </React.StrictMode>
   );
 };
